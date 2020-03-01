2e0c78adf5c400c00fb4b9ff062008d8e5cda696
Lingyi Xu <lingyixu@bu.edu>
2020-03-01 17:25:05 -0500

    Add files via upload

---
+++ Kubernetes/main.py
@@ -0,0 +1,17 @@
+#from flask import Flask
+#app = Flask(__name__)
+import tiling
+import glob
+from google.cloud import storage
+
+#@app.route("/")
+#def hello():  
+bucket_name = 'killthehw2'
+outdir = 'TCGA/tiles'
+outlocaldir = './image'
+img = 'TCGA-AA-3713-01Z-00-DX1.8148ACEB-7C1E-4D29-B908-F3729657EA4F.svs'
+tiling.TileSVS(img, outdir, outlocaldir, bucket_name)
+    
+    
+#if __name__ == "__main__":
+#    app.run(host='0.0.0.0')
