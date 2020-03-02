7e192e58387227415806aad80b94d31e7783a966
Lingyi Xu <lingyixu@bu.edu>
2020-03-02 12:16:31 -0500

    Update main.py

--- Kubernetes/main.py
+++ Kubernetes/main.py
@@ -6,11 +6,15 @@ from google.cloud import storage
 
 #@app.route("/")
 #def hello():  
-bucket_name = 'killthehw2'
+bucket_name = 'deep-nexus'
 outdir = 'TCGA/tiles'
 outlocaldir = './image'
-img = 'TCGA-AA-3713-01Z-00-DX1.8148ACEB-7C1E-4D29-B908-F3729657EA4F.svs'
-tiling.TileSVS(img, outdir, outlocaldir, bucket_name)
+
+img_list = glob.glob('slides/*.svs')
+# img = 'TCGA-AA-3713-01Z-00-DX1.8148ACEB-7C1E-4D29-B908-F3729657EA4F.svs'
+
+for img in img_list:
+    tiling.TileSVS(img, outdir, outlocaldir, bucket_name)
     
     
 #if __name__ == "__main__":
